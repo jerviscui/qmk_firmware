b23c971631832d1dca454afdbc3cca99a1df3313
jerviscui <cuijervis@126.com>
2022-12-19 01:19:34 +0800

    Update

--- keyboards/handwired/dactyl_manuform/5x7_2/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7_2/keymaps/default/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    KC_PDOT,  KC_P7,      KC_P8,   KC_P9,   KC_PAST,   KC_PMNS,                         XXXXXXX,   KC_HOME, KC_END,    KC_UP,   XXXXXXX,    KC_PGUP,    XXXXXXX,
         _______,    KC_P0,    KC_P4,      KC_P5,   KC_P6,   KC_PSLS,   KC_PPLS,                         XXXXXXX,   XXXXXXX, KC_LEFT,   KC_DOWN, KC_RGHT,    XXXXXXX,    XXXXXXX,
         _______,    KC_NO,    KC_P1,      KC_P2,   KC_P3,   KC_PEQL,                                               KC_PGUP, XXXXXXX,   XXXXXXX, XXXXXXX,    XXXXXXX,    QK_BOOT,
-        _______,    _______,  _______,    _______,                                                                                     XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,
+        _______,    _______,  _______,    _______,                                                                                     _______, _______,    _______,    _______,
                                                    _______, _______,                                               _______, _______,
                                                                        _______, _______,       _______, _______,
                                                                        _______, _______,       _______, _______
