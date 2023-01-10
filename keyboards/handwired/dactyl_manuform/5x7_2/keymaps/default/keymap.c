294ad5e531dd9ad8115b5af6612df4054862b5bd
jerviscui <cuijervis@126.com>
2023-01-11 00:54:23 +0800

    Update keymap

--- keyboards/handwired/dactyl_manuform/5x7_2/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7_2/keymaps/default/keymap.c
@@ -11,6 +11,7 @@ enum custom_layer {
 };
 
 // Some basic macros
+#define SWITCH LALT(KC_TAB)
 #define TASK   LCTL(LSFT(KC_ESC))
 #define TAB_R  LCTL(KC_TAB)
 #define TAB_L  LCTL(LSFT(KC_TAB))
@@ -27,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x7_2(
         KC_ESC,     KC_1,    KC_2,      KC_3,   KC_4,   KC_5,   KC_F12,                         KC_F11,     KC_6,    KC_7,   KC_8,       KC_9,     KC_0,     KC_GRV,
         KC_TAB,     KC_Q,    KC_W,      KC_E,   KC_R,   KC_T,   KC_MINS,                        KC_LBRC,    KC_Y,    KC_U,   KC_I,       KC_O,     KC_P,     KC_BSLS,
-        KC_LCTL,    KC_A,    KC_S,      KC_D,   KC_F,   KC_G,   KC_EQL,                         KC_RBRC,    KC_H,    KC_J,   KC_K,       KC_L,     KC_SCLN,  KC_QUOT,
+        SWITCH,     KC_A,    KC_S,      KC_D,   KC_F,   KC_G,   KC_EQL,                         KC_RBRC,    KC_H,    KC_J,   KC_K,       KC_L,     KC_SCLN,  KC_QUOT,
         OSMLS,      KC_Z,    KC_X,      KC_C,   KC_V,   KC_B,                                               KC_N,    KC_M,   KC_COMM,    KC_DOT,   KC_SLSH,  OSMRS,
         CW_TOGG,    L2,      OSMWin,    L1,                                                                                  KC_UP,  KC_DOWN,    KC_LEFT,  KC_RGHT,
                                                 KC_SPC, KC_DEL,                                             KC_BSPC, KC_ENT,
@@ -51,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    _______,  _______,    _______,   _______,   _______,   _______,                         _______,   _______, _______,   _______, _______,    _______,    _______,
         _______,    _______,  _______,    _______,   _______,   _______,   _______,                         _______,   _______, _______,   _______, _______,    _______,    _______,
         _______,    _______,  _______,    _______,   _______,   _______,                                               _______, _______,   _______, _______,    _______,    _______,
-        QK_BOOT,    _______,  _______,    _______,                                                                                         DM_REC1, DM_PLY1,    DM_REC2,    DM_PLY2,
+        _______,    _______,  QK_BOOT,    _______,                                                                                         DM_REC1, DM_PLY1,    DM_REC2,    DM_PLY2,
                                                      _______, _______,                                                 _______, _______,
                                                                            _______, _______,       _______, _______,
                                                                            _______, _______,       _______, _______
