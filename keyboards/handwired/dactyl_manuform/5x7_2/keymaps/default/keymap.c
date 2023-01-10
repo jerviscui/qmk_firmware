e598ca20fc79954824fa733b34d3e9f49e71b57d
jerviscui <cuijervis@126.com>
2023-01-11 01:07:50 +0800

    Update keymap

--- keyboards/handwired/dactyl_manuform/5x7_2/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7_2/keymaps/default/keymap.c
@@ -21,19 +21,21 @@ enum custom_layer {
 #define L2 TG(_FN)
 
 #define OSMLS OSM(MOD_LSFT)
-#define OSMWin OSM(MOD_LGUI)
 #define OSMRS OSM(MOD_RSFT)
+#define OSMLA OSM(MOD_LALT)
+#define OSMRA OSM(MOD_RALT)
+#define OSMWin OSM(MOD_LGUI)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x7_2(
         KC_ESC,     KC_1,    KC_2,      KC_3,   KC_4,   KC_5,   KC_F12,                         KC_F11,     KC_6,    KC_7,   KC_8,       KC_9,     KC_0,     KC_GRV,
         KC_TAB,     KC_Q,    KC_W,      KC_E,   KC_R,   KC_T,   KC_MINS,                        KC_LBRC,    KC_Y,    KC_U,   KC_I,       KC_O,     KC_P,     KC_BSLS,
         SWITCH,     KC_A,    KC_S,      KC_D,   KC_F,   KC_G,   KC_EQL,                         KC_RBRC,    KC_H,    KC_J,   KC_K,       KC_L,     KC_SCLN,  KC_QUOT,
-        OSMLS,      KC_Z,    KC_X,      KC_C,   KC_V,   KC_B,                                               KC_N,    KC_M,   KC_COMM,    KC_DOT,   KC_SLSH,  OSMRS,
-        CW_TOGG,    L2,      OSMWin,    L1,                                                                                  KC_UP,  KC_DOWN,    KC_LEFT,  KC_RGHT,
+        KC_LSFT,    KC_Z,    KC_X,      KC_C,   KC_V,   KC_B,                                               KC_N,    KC_M,   KC_COMM,    KC_DOT,   KC_SLSH,  KC_RSFT,
+        CW_TOGG,    L2,      OSMWin,    L1,                                                                                  KC_UP,      KC_DOWN,  KC_LEFT,  KC_RGHT,
                                                 KC_SPC, KC_DEL,                                             KC_BSPC, KC_ENT,
                                                                 KC_LCTL, KC_ENT,       KC_PGUP, KC_RCTL,
-                                                                KC_LALT, SH_TG,        KC_PGDN, KC_RALT
+                                                                OSMLA,   SH_TG,        KC_PGDN, OSMRA
     ),
 
     [_NUMPAD] = LAYOUT_5x7_2(
