b434a340272acc6a73ae8cb802ab3790fea424c0
jerviscui <cuijervis@126.com>
2023-02-08 22:19:05 +0800

    Update keymap

--- keyboards/handwired/dactyl_manuform/5x6_3_2/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_3_2/keymaps/default/keymap.c
@@ -17,8 +17,8 @@ enum custom_layer {
 #define TAB_L  LCTL(LSFT(KC_TAB))
 #define TAB_RO LCTL(LSFT(KC_T))
 
-#define L1 TG(_NUMPAD)
-#define L2 TG(_FN)
+#define L1 TG(_FN)
+#define L2 TG(_NUMPAD)
 
 #define OSMLS OSM(MOD_LSFT)
 #define OSMRS OSM(MOD_RSFT)
@@ -31,34 +31,34 @@ enum custom_layer {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6_3_2(
         KC_ESC,     KC_1,    KC_2,      KC_3,   KC_4,   KC_5,   KC_F12,                         KC_F11,     KC_6,    KC_7,   KC_8,       KC_9,     KC_0,     KC_GRV,
-        KC_TAB,     KC_Q,    KC_W,      KC_E,   KC_R,   KC_T,   KC_MINS,                        KC_LBRC,    KC_Y,    KC_U,   KC_I,       KC_O,     KC_P,     KC_BSLS,
-        SWITCH,     KC_A,    KC_S,      KC_D,   KC_F,   KC_G,   KC_EQL,                         KC_RBRC,    KC_H,    KC_J,   KC_K,       KC_L,     KC_SCLN,  KC_QUOT,
+        SWITCH,     KC_Q,    KC_W,      KC_E,   KC_R,   KC_T,   KC_MINS,                        KC_LBRC,    KC_Y,    KC_U,   KC_I,       KC_O,     KC_P,     KC_BSLS,
+        KC_TAB,     KC_A,    KC_S,      KC_D,   KC_F,   KC_G,   KC_EQL,                         KC_RBRC,    KC_H,    KC_J,   KC_K,       KC_L,     KC_SCLN,  KC_QUOT,
         OSMLS,      KC_Z,    KC_X,      KC_C,   KC_V,   KC_B,                                               KC_N,    KC_M,   KC_COMM,    KC_DOT,   KC_SLSH,  OSMRS,
         CW_TOGG,    L2,      OSMWin,    L1,                                                                                  KC_UP,      KC_DOWN,  KC_LEFT,  KC_RGHT,
                                                 KC_SPC, KC_DEL,                                             KC_BSPC, KC_ENT,
                                                                 OSMLA,  OSMLC,         OSMRC,   OSMRA,
-                                                                SH_TG,  KC_ENT,        KC_PGUP, KC_PGDN
-    ),
-
-    [_NUMPAD] = LAYOUT_5x6_3_2(
-        _______,    KC_F1,    KC_F2,      KC_F3,   KC_F4,   KC_F5,     _______,                         _______,   KC_F6,   KC_F7,     KC_F8,   KC_F9,      KC_F10,     XXXXXXX,
-        _______,    KC_PDOT,  KC_P7,      KC_P8,   KC_P9,   KC_PSLS,   KC_PAST,                         XXXXXXX,   KC_HOME, KC_END,    KC_UP,   XXXXXXX,    KC_PGUP,    XXXXXXX,
-        _______,    KC_P0,    KC_P4,      KC_P5,   KC_P6,   KC_PMNS,   KC_PPLS,                         XXXXXXX,   XXXXXXX, KC_LEFT,   KC_DOWN, KC_RGHT,    XXXXXXX,    XXXXXXX,
-        _______,    XXXXXXX,  KC_P1,      KC_P2,   KC_P3,   KC_PEQL,                                               KC_PGDN, XXXXXXX,   XXXXXXX, XXXXXXX,    XXXXXXX,    _______,
-        _______,    _______,  _______,    _______,                                                                                     _______, _______,    _______,    _______,
-                                                   _______, _______,                                               _______, _______,
-                                                                       _______, _______,       _______, _______,
-                                                                       _______, _______,       KC_PSCR, KC_INS
+                                                                SH_TG,  KC_ENT,        KC_PSCR, KC_INS
     ),
 
     [_FN] = LAYOUT_5x6_3_2(
-        DB_TOGG,    _______,  _______,    _______,   _______,   _______,   _______,                         _______,   _______, _______,   _______, _______,    _______,    _______,
+        _______,    KC_F1,    KC_F2,      KC_F3,     KC_F4,     KC_F5,     _______,                         _______,   KC_F6,   KC_F7,     KC_F8,   KC_F9,      KC_F10,     _______,
         _______,    _______,  _______,    _______,   _______,   _______,   _______,                         _______,   _______, _______,   _______, _______,    _______,    _______,
         _______,    _______,  _______,    _______,   _______,   _______,   _______,                         _______,   _______, _______,   _______, _______,    _______,    _______,
         _______,    _______,  _______,    _______,   _______,   _______,                                               _______, _______,   _______, _______,    _______,    _______,
-        _______,    _______,  QK_BOOT,    _______,                                                                                         DM_REC1, DM_PLY1,    DM_REC2,    DM_PLY2,
-                                                     _______, _______,                                                 _______, _______,
+        _______,    _______,  _______,    _______,                                                                                         _______, _______,    _______,    _______,
+                                                     _______,   _______,                                               _______, _______,
                                                                            _______, _______,       _______, _______,
                                                                            _______, _______,       _______, _______
+    ),
+
+    [_NUMPAD] = LAYOUT_5x6_3_2(
+        _______,    KC_F1,    KC_F2,      KC_F3,   KC_F4,   KC_F5,     _______,                         _______,   KC_F6,     KC_F7,     KC_F8,     KC_F9,      KC_F10,     XXXXXXX,
+        _______,    KC_PDOT,  KC_P7,      KC_P8,   KC_P9,   KC_PSLS,   KC_PMNS,                         XXXXXXX,   KC_HOME,   KC_END,    KC_UP,     XXXXXXX,    KC_PGUP,    XXXXXXX,
+        _______,    KC_P0,    KC_P4,      KC_P5,   KC_P6,   KC_PAST,   KC_PPLS,                         XXXXXXX,   XXXXXXX,   KC_LEFT,   KC_DOWN,   KC_RGHT,    XXXXXXX,    XXXXXXX,
+        _______,    XXXXXXX,  KC_P1,      KC_P2,   KC_P3,   XXXXXXX,                                               KC_PGDN,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    _______,
+        _______,    _______,  QK_BOOT,    _______,                                                                                       DM_REC1,   DM_PLY1,    DM_REC2,    DM_PLY2,
+                                                   _______, _______,                                               _______, _______,
+                                                                       _______, _______,       _______, _______,
+                                                                       _______, _______,       _______, _______
     )
 };
